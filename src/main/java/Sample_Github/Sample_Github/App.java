58c66fa9b2d0d3a6f7504934f0dd27b6b3dbfbc8
Ameena081 <ameenakhatun23@gmail.com>
2022-03-09 16:13:45 +0530

    This is my first commit

---
+++ src/main/java/Sample_Github/Sample_Github/App.java
@@ -0,0 +1,13 @@
+package Sample_Github.Sample_Github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/Merging/Sample_1.java
@@ -0,0 +1,10 @@
+package com.Merging;
+
+public class Sample_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Sad Day");
+
+	}
+
+}
